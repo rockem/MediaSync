23f27c72d50d23a4257673130fd35461eecc35bb
Eli Segal <eli.segal@gmail.com>
2015-01-03 22:15:20 +0200

    Updated synching options

--- src/audio_file_info.py
+++ src/audio_file_info.py
@@ -1,18 +1,64 @@
 import os
+from types import NoneType
+from mutagen.easyid3 import EasyID3
 from mutagen.m4a import M4A
-from mutagen.mp3 import MP3
 
 
 class UnsupportedFileError(IOError):
     pass
 
 
-class AudioFileInfo(object):
-
-    mime_to_mutagen_info = {"mp3": MP3, "m4a": M4A}
+class MP3AudioInfo:
 
     def __init__(self, file_path):
-        mutagen_info = self.mime_to_mutagen_info[self.get_ext_for(file_path)]
+        self.audio_info = EasyID3(file_path)
+
+    def get_info(self):
+        return str(self.audio_info.Get)
+
+    def get_value_for(self, tag_name):
+        if tag_name == "album":
+            return self.audio_info["album"][0]
+
+        if tag_name == "genre":
+            try:
+                return self.audio_info["genre"][0]
+            except KeyError:
+                pass
+
+        return ""
+
+
+class M4AAudioInfo:
+
+    def __init__(self, file_path):
+        self.audio_info = M4A(file_path)
+
+    def get_info(self):
+        return str(self.audio_info.tags)
+
+    def get_value_for(self, tag_name):
+        audio_tags = self.audio_info.tags
+        try:
+            if tag_name == "album":
+                return audio_tags['\xa9alb']
+            if tag_name == "genre":
+                return audio_tags['\xa9gen']
+        except(TypeError, KeyError):
+            pass
+
+        return ""
+
+
+class AudioFileInfo(object):
+
+    mime_to_mutagen_info = {"mp3": MP3AudioInfo, "m4a": M4AAudioInfo}
+
+    def __init__(self, file_path):
+        mutagen_info = None
+        ext = self.get_ext_for(file_path)
+        if ext in self.mime_to_mutagen_info:
+            mutagen_info = self.mime_to_mutagen_info[ext]
         if mutagen_info is None:
             raise UnsupportedFileError
 
@@ -21,11 +67,10 @@ class AudioFileInfo(object):
     def get_ext_for(self, file_path):
         return os.path.splitext(file_path)[1].replace(".", "")
 
-    def get_value_for(self, property):
-        if property == "bitrate":
-            return self.audio_info.info.bitrate
+    def get_value_for(self, tag_name):
+        return self.audio_info.get_value_for(tag_name)
 
-        if property == "genre":
-            return self.audio_info.info.genre
+    def get_info(self):
+        return self.audio_info.get_info()
 
-        return ""
+
--- src/audiosync.py
+++ src/audiosync.py
@@ -1,10 +1,11 @@
 # coding=utf-8
 import hashlib
 import os
+import shutil
 import sys
 
 from audio_file_info import AudioFileInfo
-from src.audio_file_info import UnsupportedFileError
+from audio_file_info import UnsupportedFileError
 
 
 class FileSync:
@@ -12,23 +13,29 @@ class FileSync:
         self.source = source
         self.target = target
         self.max_bitrate = 320
-        self.genres_to_filter = "Practice"
+        self.genres_to_filter = ["Practice", "Voice Memo"]
+        self.files_to_ignore = ["._.DS_Store", ".DS_Store"]
         print "Copying from %s to %s ..." % (source, target)
 
     def sync(self):
+        #self.delete_removed_files()
+        print "Copying from %s to %s ..." % (self.source, self.target)
         for root, dirs, filenames in os.walk(self.source):
             for f in filenames:
                 f_path = os.path.join(root, f)
                 if os.path.isfile(f_path):
                     try:
                         current_file_info = AudioFileInfo(f_path)
-                        if self.audio_is_lossless(current_file_info) and \
+                        if self.file_is_not_mp3_version(current_file_info) and \
                                 self.genre_should_not_filtered(current_file_info) and \
                                 self.file_is_different_from_target(f_path):
                             self.sync_file(os.path.join(root, f))
                             self.write(".")
                     except UnsupportedFileError:
                         self.write("-")
+
+    def file_is_not_mp3_version(self, audio):
+        return audio.get_value_for("album").find("mp3") == -1
 
     def audio_is_lossless(self, audio):
         return audio.get_value_for("bitrate") <= self.max_bitrate
@@ -38,7 +45,10 @@ class FileSync:
         sys.stdout.flush()
 
     def genre_should_not_filtered(self, audio):
-        return not audio.get_value_for("genre") in self.genres_to_filter
+        for g in self.genres_to_filter:
+            if audio.get_value_for("genre").find(g) != -1:
+                return False
+        return True
 
     def file_is_different_from_target(self, file_path):
         target_file = self.get_target_file_for(file_path)
@@ -55,14 +65,18 @@ class FileSync:
         return os.path.join(self.target, os.path.relpath(os.path.dirname(file_path), self.source))
 
     def files_are_same(self, file_path1, file_path2):
-        return self.create_hash_for(file_path1) == self.create_hash_for(file_path2)
+        return self.files_are_same_in_size(file_path1, file_path2) and \
+            self.create_hash_from(AudioFileInfo(file_path1).get_info()) == self.create_hash_from(AudioFileInfo(file_path2).get_info())
+
+    def files_are_same_in_size(self, file_path1, file_path2):
+        return os.path.getsize(file_path1) == os.path.getsize(file_path2)
 
     def create_hash_for(self, file_path):
         md5 = hashlib.md5()
         with open(file_path, 'rb') as f:
-            for chunk in iter(lambda: f.read(8192), b''):
+            for chunk in iter(lambda: f.read(128 * md5.block_size), b''):
                 md5.update(chunk)
-            return md5.digest()
+            return md5.hexdigest()
 
     def sync_file(self, file_path):
         target_path = self.get_target_path_for(file_path)
@@ -72,9 +86,24 @@ class FileSync:
         os.system('cp "%s" "%s"' % (file_path, target_path))
         # shutil.copyfile(file_path, self.get_target_file_for(file_path))
 
+    def create_hash_from(self, param):
+        md5 = hashlib.md5()
+        md5.update(param)
+        return md5.hexdigest()
+
+    def delete_removed_files(self):
+        for root, dirs, file_names in os.walk(self.target):
+            if not os.path.exists(os.path.join(self.source, root.replace(self.target, "").replace("/", "", 1))):
+                shutil.rmtree(root)
+            else:
+                for f in file_names:
+                    if not os.path.split(f)[1] in self.files_to_ignore and \
+                            not os.path.exists(os.path.join(self.source, f)):
+                        os.remove(os.path.join(self.target, f))
+
 
 if __name__ == "__main__":
     file_sync = FileSync(
-        os.path.abspath("/Volumes/My Book TM/Backups.backupdb/Eli’s iMac/Latest/Lacie Extra/iTunes Music/Music"),
-        os.path.abspath("/Volumes/Xtreamer_PRO/sda1/Music"))
+        os.path.abspath("/Volumes/Lacie Extra/iTunes Music/Music"),
+        os.path.abspath("/Volumes/Music"))
     file_sync.sync()
